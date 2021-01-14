48b25e052ca2de3f5840f9ced87ee4da791071f9
dev-bwayne <dev.bwayne@gmail.com>
2021-01-14 16:47:47 +0600

    Enhanced API for sales data handler.

--- umag/sales_prediction/src/main/java/kz/umag/hackathons/sales_prediction/api/SalesDataHandler.java
+++ umag/sales_prediction/src/main/java/kz/umag/hackathons/sales_prediction/api/SalesDataHandler.java
@@ -1,5 +1,8 @@
 package kz.umag.hackathons.sales_prediction.api;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSortedMap;
+
 import java.time.LocalDate;
 import java.util.OptionalDouble;
 import java.util.OptionalInt;
@@ -27,4 +30,25 @@ public interface SalesDataHandler {
    * @return empty if {@code storeId} and/or {@code productBarcode} is not present in the database.
    */
   OptionalDouble getSalesMean(Long storeId, Long productBarcode);
+
+  /**
+   * Returns a list of all stores (IDs) present in the database.
+   */
+  ImmutableSet<Long> getAllStores();
+
+  /**
+   * Returns a list of all products (barcodes) for the corresponding store present in the database.
+   *
+   * <p>Warning: might return empty when the store is not present in the database or has no
+   * products.</p>
+   */
+  ImmutableSet<Long> getAllProductsOfStore(Long storeId);
+
+  /**
+   * Returns a history of sales for the given store and product sorted by date.
+   *
+   * <p>Warning: might return empty when the store-product pair is not present in the database or
+   * has no no history.</p>
+   */
+  ImmutableSortedMap<LocalDate, Integer> getSalesSeries(Long storeId, Long productBarcode);
 }
