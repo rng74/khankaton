fc14dfd5b1412c22b8dd46110eb01636784fa181
rng74 <ers98ers@gmail.com>
2021-01-27 14:17:17 +0600

    Removed useless part of code

--- umag/sales_prediction/src/main/java/kz/umag/hackathons/sales_prediction/impl/CustomPredictor.java
+++ umag/sales_prediction/src/main/java/kz/umag/hackathons/sales_prediction/impl/CustomPredictor.java
@@ -1,20 +1,11 @@
 package kz.umag.hackathons.sales_prediction.impl;
 
-import com.google.common.collect.ImmutableSortedMap;
-import de.bwaldvogel.liblinear.*;
 import kz.umag.hackathons.sales_prediction.api.SalesDataHandler;
 import kz.umag.hackathons.sales_prediction.api.SalesPredictor;
 
 import javax.inject.Inject;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.OptionalInt;
-import java.util.stream.LongStream;
 
 /**
  * Implementation of {@link SalesPredictor} that makes custom predictions.
@@ -22,6 +13,7 @@ import java.util.stream.LongStream;
 class CustomPredictor implements SalesPredictor {
 
     private static final int PREDICTION_WINDOW = 5;
+    private static final int DEFAULT_VAL = 0;
 
     private final SalesDataHandler salesDataHandler;
 
@@ -32,53 +24,21 @@ class CustomPredictor implements SalesPredictor {
 
     @Override
     public OptionalInt predict(Long storeId, Long productBarcode, LocalDate curDate) {
-
-//        try {
-//            BufferedWriter writer = new BufferedWriter(new FileWriter("D://data_test.csv", true));
-//            writer.write(storeId + " " + productBarcode + " " + curDate.toString());
-//            writer.newLine();
-//            writer.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
         if (!salesDataHandler.hasData(storeId, productBarcode))
             return OptionalInt.empty();
-/*   this is ML predictor
-        Model model;
-        File modelFile = new File("D://model2");
-        double result = 0f;
-
-        try {
-            model = Model.load(modelFile.toPath());
-
-            for (int i = 0; i < PREDICTION_WINDOW; i++) {
-                Feature[] instance = {
-                        new FeatureNode(1, storeId.intValue()),
-                        new FeatureNode(2, productBarcode.intValue()),
-                        new FeatureNode(3, curDate.plusDays(i).getDayOfWeek().getValue()),
-                        new FeatureNode(4, curDate.plusDays(i).getDayOfMonth())
-                };
-                double predictedAmount = Linear.predict(model, instance);
-                System.out.println("!CHECK! - " + salesDataHandler.getSales(storeId, productBarcode, curDate) + " || predictedAmount - " + predictedAmount);
-                result += predictedAmount;
-            }
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
-
-        return OptionalInt.of((int) Math.round(result));*/
 
         double result = 0.0f;
-        double[] coef = {0.3, 0.2, 0.125, 0.1, 0.062, 0.05, 0.025, 0.012, 0.001};
+        double[] magicCoefficients = {0.34, 0.16, 0.12, 0.09, 0.062, 0.05, 0.02, 0.02, 0.01}; // 0.375
 
-        long curM = 0L;
-        for (int i = 0, j = 0; j < coef.length; i++) {
-            curM += salesDataHandler.getSales(storeId, productBarcode, curDate.minusDays(i)).orElse(0);
-            if ((i + 1) % 5 == 0) {
-                result += (coef[j] * curM);
+        long curWeek = DEFAULT_VAL;
+        for (int i = 0, j = 0; j < magicCoefficients.length; i++) {
+            curWeek += salesDataHandler
+                    .getSales(storeId, productBarcode, curDate.minusDays(i))
+                    .orElse(DEFAULT_VAL);
+            if ((i + 1) % PREDICTION_WINDOW == 0) {
+                result += (magicCoefficients[j] * curWeek);
                 j++;
-                curM = 0;
+                curWeek = DEFAULT_VAL;
             }
         }
         return OptionalInt.of((int) result);
